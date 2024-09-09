25bd71ee33d4fa5bae43c0aee578b51a25538cab
tangthiendat <thiendattang@gmail.com>
2024-09-09 20:30:57 +0700

    feat: add permission ui

--- src/features/auth/AddPermission.tsx
+++ src/features/auth/AddPermission.tsx
@@ -1,11 +1,40 @@
 import { PlusOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import { Button, Form, Modal } from "antd";
+import { useState } from "react";
+import UpdatePermissionForm from "./UpdatePermissionForm";
+import { IPermission } from "../../interfaces";
+
 const AddPermission: React.FC = () => {
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const [addPermissionForm] = Form.useForm<IPermission>();
+
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+    addPermissionForm.resetFields();
+  };
+
   return (
     <>
-      <Button type="primary" icon={<PlusOutlined />}>
+      <Button type="primary" icon={<PlusOutlined />} onClick={handleOpenModal}>
         Thêm mới
       </Button>
+      <Modal
+        open={isOpenModal}
+        width="30%"
+        title={<span className="text-lg">Thêm quyền hạn</span>}
+        destroyOnClose
+        onCancel={handleCloseModal}
+        footer={null}
+      >
+        <UpdatePermissionForm
+          form={addPermissionForm}
+          onCancel={handleCloseModal}
+        />
+      </Modal>
     </>
   );
 };
