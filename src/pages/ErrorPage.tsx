f4f6268c3a3af3ed4540e4f565cb902756e2041e
Tang Thien Dat <thiendattang@gmail.com>
2024-10-30 10:37:52 +0700

    style: fix sorted column color and center error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -21,19 +21,21 @@ const ErrorPage: React.FC = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center text-center">
-      <img
-        src="/sth-went-wrong.png"
-        alt="Something went wrong"
-        width={300}
-        height={300}
-      />
+    <div className="flex min-h-screen flex-col">
+      <div className="flex flex-1 flex-col items-center justify-center text-center">
+        <img
+          src="/sth-went-wrong.png"
+          alt="Something went wrong"
+          width={300}
+          height={300}
+        />
 
-      <p className="my-4">{errorMessage}</p>
+        <p className="my-4">{errorMessage}</p>
 
-      <Button size="large" type="primary" onClick={() => navigate("/")}>
-        Trở về trang chủ
-      </Button>
+        <Button size="large" type="primary" onClick={() => navigate("/")}>
+          Trở về trang chủ
+        </Button>
+      </div>
     </div>
   );
 };
