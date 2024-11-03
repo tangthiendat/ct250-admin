a28fa37ab444416f2ac9598ecb97481e7620263d
Tang Thien Dat <thiendattang@gmail.com>
2024-11-03 12:47:45 +0700

    style(flight): hide pagination on single page

--- src/features/flight/schedule/FlightPanel.tsx
+++ src/features/flight/schedule/FlightPanel.tsx
@@ -65,7 +65,7 @@ const FlightPanel: React.FC = () => {
       </div>
 
       <FlightList flights={data?.payload?.content || []} />
-      <Pagination {...pagination} />
+      <Pagination {...pagination} hideOnSinglePage />
     </div>
   );
 };
